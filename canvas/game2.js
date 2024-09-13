5350203da6f55ada6772601fb0085b0463f5d457
Joncarlos <164406145+JoncarlosReyes@users.noreply.github.com>
2024-09-13 10:46:45 -0400

    Update game2.js

--- canvas/game2.js
+++ canvas/game2.js
@@ -1,1 +1,4 @@
+var backgroundMusic = new Audio('jam-spotlight-lazytoms-|-29565.mp3');
+backgroundMusic.loop = true;  // This will loop the music
+backgroundMusic.volume = 0.5; // Adjust volume (optional)
 
